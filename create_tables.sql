46c8afe08fae7e5ab9cd0a38b3590721f9b4ff6e
Laye 'Leo' Mehta <layeamehta@gmail.com>
2021-12-23 02:08:32 -0500

    modified create tables script and new load tables script

--- create_tables.sql
+++ create_tables.sql
@@ -1,42 +1,51 @@
 CREATE TABLE company (
     ticker varchar(5) NOT NULL,
     name varchar(255) NOT NULL,
-    market_cap int NOT NULL,
+    market_cap bigint NOT NULL,
     sector varchar(255),
     industry varchar(255),
-    city varchar(255),
+    city varchar(255) NOT NULL,
     state varchar(255),
     PRIMARY KEY (ticker)
 );
+
 CREATE TABLE stock_info (
     ticker varchar(5) NOT NULL,
     date DATETIME NOT NULL,
-    volume int NOT NULL,
-    open int,
-    high int,
-    low int,
-    close int,
-    adj_close int,
+    volume bigint NOT NULL,
+    open float,
+    high float,
+    low float,
+    close float,
+    adj_close float,
     FOREIGN KEY (ticker) REFERENCES company(ticker)
 );
+
+CREATE TABLE user (
+    user_id int NOT NULL,
+    name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
+    user_name varchar(255) NOT NULL,
+    PRIMARY KEY (user_id)
 );
+
 CREATE TABLE comment (
     comment_id int NOT NULL,
     date DATETIME NOT NULL,
     ticker varchar(5) NOT NULL,
     content varchar(255),
     commenter_id int,
-    PRIMARY KEY (commentId),
-    FOREIGN KEY (ticker) REFERENCES company(ticker),
+    PRIMARY KEY (comment_id),
     FOREIGN KEY (commenter_id) REFERENCES user(user_id)
 );
-CREATE TABLE user (
-    user_id int NOT NULL,
-    name varchar(255) NOT NULL,
-    last_name varchar(255) NOT NULL,
-    user_name varchar(255) NOT NULL,
-    PRIMARY KEY (user_id),
+
+CREATE TABLE publisher (
+    publisher_id int NOT NULL,
+    name text NOT NULL,
+    url text NOT NULL,
+    PRIMARY KEY (publisher_id)
 );
+
 CREATE TABLE news (
     news_id int NOT NULL,
     ticker varchar(5) NOT NULL,
@@ -44,51 +53,60 @@ CREATE TABLE news (
     title varchar(255),
     publisher_id int NOT NULL,
     PRIMARY KEY (news_id),
-    FOREIGN KEY (ticker) REFERENCES company(ticker),
     FOREIGN KEY (publisher_id) REFERENCES publisher(publisher_id)
+);
 
-);
-CREATE TABLE publisher (
-    publisher_id int NOT NULL,
-    name varchar(255) NOT NULL,
-    url varchar(255) NOT NULL,
-    PRIMARY KEY (publisher_d),
-);
-CREATE TABLE managment (
+CREATE TABLE management (
     ticker varchar(5) NOT NULL,
     ceo_name varchar(255) NOT NULL,
+    ceo_age int NOT NULL,
     ceo_gender varchar(255) NOT NULL,
     president_name varchar(255) NOT NULL,
+    president_age int NOT NULL,
     president_gender varchar(255) NOT NULL,
-    city varchar(255) NOT NULL,
-    state varchar(255) NOT NULL,
-    PRIMARY KEY (ticker) REFERENCES company(ticker),
-    FOREIGN KEY (ticker) REFERENCES company(ticker),
+    FOREIGN KEY (ticker) REFERENCES company(ticker)
 );
+
 CREATE TABLE ipo_data (
     ticker varchar(5) NOT NULL,
     ipo_date DATE NOT NULL,
-    first_day_high int,
-    first_day_low int,
-    first_day_open int,
-    first_day_close int,
-    first_day_volume int,
-    PRIMARY KEY (ticker) REFERENCES company(ticker),
+    first_day_high float,
+    first_day_low float,
+    first_day_open float,
+    first_day_close float,
+    first_day_volume float,
     FOREIGN KEY (ticker) REFERENCES company(ticker)
 );
-CREATE TABLE finantial_info (
+
+CREATE TABLE financial_info (
     ticker varchar(5) NOT NULL,
-    liabilities DATE NOT NULL,
-    assets int,
-    property_investment int,
-    capital_expenditure int,
-    debt int,
-    debt_repayment int,
-    revenue int,
-    PRIMARY KEY (ticker) REFERENCES company(ticker),
-    FOREIGN KEY (ticker) REFERENCES company(ticker),
+    liabilities bigint NOT NULL,
+    assets bigint,
+    property_investment bigint,
+    capital_expenditure bigint,
+    debt bigint,
+    debt_repayment bigint,
+    revenue bigint,
+    FOREIGN KEY (ticker) REFERENCES company(ticker)
 );
+
 CREATE TABLE trend_info (
     ticker varchar(5) NOT NULL,
-    date DATE,
+    monthTrend int, 
+    threeMonthTrend int, 
+    sixMonthTrend int, 
+    yearTrend int, 
+    FOREIGN KEY (ticker) REFERENCES company(ticker)
 );
+
+CREATE TABLE watchlist (
+    trade_id int, 
+    user text, 
+    ticker varchar(5), 
+    date date, 
+    price float, 
+    amount int, 
+    total_price float,
+    PRIMARY KEY (trade_id),
+    FOREIGN KEY (ticker) REFERENCES company(ticker)
+); 
---
+++ load_tables.sql
@@ -0,0 +1,77 @@
+-- load company
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/company.csv' INTO TABLE company 
+    FIELDS TERMINATED BY ','
+    ENCLOSED BY '"' 
+    ESCAPED BY '\\'
+    LINES TERMINATED BY '\n'
+    IGNORE 1 LINES 
+    (@dummy, @tick, @name, @market_cap, @sector, @industry, @city, @stateCountry)
+    SET ticker=@tick, name=@name, market_cap=@market_cap, sector=@sector, industry=@industry, city=@city, state=@stateCountry;  
+
+-- load stock_info
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/fh_5yrs.csv' INTO TABLE stock_info 
+    FIELDS TERMINATED BY ','
+    LINES TERMINATED BY '\n'
+    IGNORE 1 LINES 
+    (@date, @vol, @op, @hi, @lo, @cls, @adjcls, @tick)
+    SET ticker=@tick, date=@date, volume=@vol, open=@op, high=@hi, low=@lo, close=@cls, adj_close=@adjcls;    
+
+-- load publisher
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/publisher.csv' INTO TABLE publisher
+    FIELDS TERMINATED BY ','
+    ENCLOSED BY '"' 
+    ESCAPED BY '\\'
+    LINES TERMINATED BY '\n'
+    IGNORE 1 LINES 
+    (@publisher_id, @url, @name)
+    SET publisher_id=@publisher_id, name=@name, url=@url; 
+
+-- load news
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/news.csv' INTO TABLE news
+    FIELDS TERMINATED BY ','
+    ENCLOSED BY '"' 
+    ESCAPED BY '\\'
+    LINES TERMINATED BY '\n'
+    IGNORE 1 LINES 
+    (@news_id, @headline, @dummy, @date, @ticker)
+    SET news_id=@news_id, ticker=@ticker, date=@date, title=@headline, publisher_id=@news_id; 
+
+-- load management
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/management.csv' INTO TABLE management
+    FIELDS TERMINATED BY ','
+    ENCLOSED BY '"' 
+    ESCAPED BY '\\'
+    LINES TERMINATED BY '\n'
+    IGNORE 1 LINES 
+    (@dummy, @symbol, @ceo_age, @ceo_name, @ceo_gender, @pres_age, @pres_name, @pres_gender)
+    SET ticker=@symbol, ceo_name=@ceo_name, ceo_name=@ceo_name, ceo_gender=@ceo_gender, president_name=@pres_name, president_age=@pres_age, president_gender=@pres_gender; 
+
+-- load ipo_data
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/ipo_data.csv' INTO TABLE ipo_data
+    FIELDS TERMINATED BY ','
+    ENCLOSED BY '"' 
+    ESCAPED BY '\\'
+    LINES TERMINATED BY '\n'
+    IGNORE 1 LINES 
+    (@dummy, @symbol, @close, @hi, @open, @lo, @volume, @date)
+    SET ticker=@symbol, ipo_date=@date, first_day_high=@hi, first_day_low=@lo, first_day_open=@open, first_day_close=@close, first_day_volume=@volume;
+
+-- load financial_info
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/financial_health.csv' INTO TABLE financial_info
+    FIELDS TERMINATED BY ','
+    ENCLOSED BY '"' 
+    ESCAPED BY '\\'
+    LINES TERMINATED BY '\n'
+    IGNORE 1 LINES 
+    (@dummy, @symbol, @assets, @liab, @prop_inv, @cap_exp, @debt, @debt_rep, @revenue)
+    SET ticker=@symbol, liabilities=@liab, assets=@assets, property_investment=@prop_inv, capital_expenditure=@cap_exp, debt=@debt, debt_repayment=@debt_rep, revenue=@revenue;
+
+-- load trend_info
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/trend_info.csv' INTO TABLE trend_info
+    FIELDS TERMINATED BY ','
+    ENCLOSED BY '"' 
+    ESCAPED BY '\\'
+    LINES TERMINATED BY '\n'
+    IGNORE 1 LINES 
+    (@dummy, @symbol, @month, @3month, @6month, @year)
+    SET ticker=@symbol, monthTrend=@month, threeMonthTrend=@3month, sixMonthTrend=@6month, yearTrend=@year;
